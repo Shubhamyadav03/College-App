7eda6efa4e043ca862629fa4c25a031cb9394cf9
Sagar Sharma <sagar.sharma_cs18@gla.ac.in>
2021-04-16 11:28:43 +0530

    retrieving data from firebase for showing teachers details
    Here we are trying to show teachers data in faculty activity

--- CollegeAppAdmin/app/src/main/java/com/example/collegeapp1/faculty/UpdateFaculty.java
+++ CollegeAppAdmin/app/src/main/java/com/example/collegeapp1/faculty/UpdateFaculty.java
@@ -1,32 +1,184 @@
 package com.example.collegeapp1.faculty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.collegeapp1.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.collegeapp1.R.*;
 
 public class UpdateFaculty extends AppCompatActivity {
 
     FloatingActionButton fab;
+    private RecyclerView csDepartment, mechDepartment, elecDepartment, electroDepartment;
+    private LinearLayout csNodata, mechNodata, elecNodata, electroNodata;
+    private List<TeacherData> list1, list2, list3, list4;
+    private DatabaseReference reference, dbRef;
+    private TeacherAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_faculty);
 
+        elecDepartment = findViewById(id.elecDepartment);
+        mechDepartment = findViewById(id.mechDepartment);
+        csDepartment = findViewById(id.csDepartment);
+        electroDepartment = findViewById(id.electroDepartment);
+
+        elecNodata = findViewById(id.elecNodata);
+        mechNodata = findViewById(id.mechNodata);
+        csNodata = findViewById(id.csNodata);
+        electroNodata = findViewById(id.electroNodata);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("teacher");
+        csDepartment();
+        mechDepartment();
+        elecDepartment();
+        electroDepartment();
+
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(UpdateFaculty.this,AddTeacher.class));
+                startActivity(new Intent(UpdateFaculty.this, AddTeacher.class));
 
 
+            }
+        });
+    }
+
+    private void csDepartment() {
+        dbRef = reference.child("Computer Science");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list1 = new ArrayList<>();
+                if (!dataSnapshot.exists()) {
+                    csNodata.setVisibility(View.VISIBLE);
+                    csDepartment.setVisibility(View.GONE);
+                } else {
+                    csNodata.setVisibility(View.GONE);
+                    csDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list1.add(data);
+                    }
+                    csDepartment.setHasFixedSize(true);
+                    csDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
+                    adapter = new TeacherAdapter(list1, UpdateFaculty.this);
+                    csDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateFaculty.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void mechDepartment() {
+        dbRef = reference.child("Mechanical");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list2 = new ArrayList<>();
+                if (!dataSnapshot.exists()) {
+                    mechNodata.setVisibility(View.VISIBLE);
+                    mechDepartment.setVisibility(View.GONE);
+                } else {
+                    mechNodata.setVisibility(View.GONE);
+                    mechDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list2.add(data);
+                    }
+                    mechDepartment.setHasFixedSize(true);
+                    mechDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
+                    adapter = new TeacherAdapter(list2, UpdateFaculty.this);
+                    mechDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateFaculty.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void elecDepartment() {
+        dbRef = reference.child("Physics");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list3 = new ArrayList<>();
+                if (!dataSnapshot.exists()) {
+                    elecNodata.setVisibility(View.VISIBLE);
+                    elecDepartment.setVisibility(View.GONE);
+                } else {
+                    elecNodata.setVisibility(View.GONE);
+                    elecDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list3.add(data);
+                    }
+                    elecDepartment.setHasFixedSize(true);
+                    elecDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
+                    adapter = new TeacherAdapter(list3, UpdateFaculty.this);
+                    elecDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateFaculty.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void electroDepartment() {
+        dbRef = reference.child("Chemistry");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list4 = new ArrayList<>();
+                if (!dataSnapshot.exists()) {
+                    electroNodata.setVisibility(View.VISIBLE);
+                    electroDepartment.setVisibility(View.GONE);
+                } else {
+                    electroDepartment.setVisibility(View.GONE);
+                    electroDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list4.add(data);
+                    }
+                    electroDepartment.setHasFixedSize(true);
+                    electroDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
+                    adapter = new TeacherAdapter(list4, UpdateFaculty.this);
+                    electroDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateFaculty.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
