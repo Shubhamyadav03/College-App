c5e6664b9f308df0def2f40fcb827835b8460a4a
Shubham Yadav <shubham.yadav_cs18@gla.ac.in>
2021-04-20 14:57:33 +0530

    Update AboutFragment.java

--- CollegeUsersApp/app/src/main/java/com/example/collegeusersapp/ui/about/AboutFragment.java
+++ CollegeUsersApp/app/src/main/java/com/example/collegeusersapp/ui/about/AboutFragment.java
@@ -1,22 +1,49 @@
 package com.example.collegeusersapp.ui.about;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.bumptech.glide.Glide;
 import com.example.collegeusersapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AboutFragment extends Fragment {
+    private ViewPager viewPager;
+    private BranchAdapter adapter;
+    private List<BranchModel> list;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        list = new ArrayList<>();
+        list.add(new BranchModel(R.drawable.ic_comp, "Computer Science", "Computer Science and Engineering started in year 2015. At present intake sheet in I-year is 30 student, and in lateral entry is 6."));
+        list.add(new BranchModel(R.drawable.ic_mach, "Mechanical Production", "Mechanical Engineering (Production) started in year 2013. At present intake sheet in I-year is 30 student, and in lateral entry is 6."));
+
+        adapter = new BranchAdapter( getContext(), list);
+
+        viewPager = view.findViewById(R.id.viewPager);
+
+        viewPager.setAdapter(adapter);
+
+        ImageView imageView = view.findViewById(R.id.college_image);
+
+        Glide.with(getContext())
+                .load("https://firebasestorage.googleapis.com/v0/b/gp-adampur-1307e.appspot.com/o/others%2Fcollege.jpg?alt=media&token=91e373ad-b93a-4156-99a7-9f6834ac9b76")
+                .into(imageView);
+
+        return  view;
     }
 }
